037aaee543d5a853e450101ac2a8e1c6117ba49e
Konrad <konrad.stochel.13@gmail.com>
2020-02-19 11:35:27 +0100

    add function for responsive slider

--- js/app.js
+++ js/app.js
@@ -1,1 +1,20 @@
 import '../sass/style.scss';
+
+document.addEventListener('DOMContentLoaded', () => {
+
+  const containerImg = document.querySelector('.images');
+  const img1 = document.querySelector('.images__img--first img');
+  const img2 = document.querySelector('.images__img--second img');
+
+
+  function adjustWidth() {
+    const containerWidth = containerImg.offsetWidth;
+
+    img1.style.width = containerWidth + 'px';
+    img2.style.width = containerWidth + 'px';
+  }
+
+  window.addEventListener('resize', adjustWidth)
+
+  adjustWidth();
+})
