29f5efcbe5fa68f4c865817a8ec68a37a2eb92e8
Konrad <konrad.stochel.13@gmail.com>
2020-02-19 12:30:38 +0100

    final commit, add functions for dragging slider

--- js/app.js
+++ js/app.js
@@ -5,10 +5,61 @@ document.addEventListener('DOMContentLoaded', () => {
   const containerImg = document.querySelector('.images');
   const img1 = document.querySelector('.images__img--first img');
   const img2 = document.querySelector('.images__img--second img');
+  let dragging = false;
+  const img1Container = document.querySelector('.images__img--first');
+  const img2Container = document.querySelector('.images__img--second');
+  const rope = document.querySelector('.images__rope');
+  const handle = document.querySelector('.images__hanlde');
+  let containerLeft;
+  let containerWidth;
+
+  function getOffset(clientX) {
+    const offset = clientX - containerLeft;
+    if (offset < 0) return 0;
+    else if (offset > containerWidth) return containerWidth;
+    else return offset;
+  }
+
+  function move(clientX) {
+    const offset = getOffset(clientX)
+    const positionRope = (offset / containerWidth * 100);
+    rope.style.left = `${positionRope}%`;
+    img2Container.style.width = `${positionRope}%`;
+  }
+
+  function initEvents() {
+    rope.addEventListener('mousedown', () => {
+      dragging = true;
+    });
+
+    rope.addEventListener('mouseup', () => {
+      dragging = false;
+    });
+
+    window.addEventListener('mousemove', (e) => {
+      if (dragging) {
+        move(e.clientX)
+      }
+    })
+    rope.addEventListener('touchstart', () => {
+      dragging = true;
+    });
+
+    rope.addEventListener('touchend', () => {
+      dragging = false;
+    });
+
+    window.addEventListener('touchmove', (e) => {
+      if (dragging) {
+        move(e.touches[0].clientX)
+      }
+    })
+  }
 
 
   function adjustWidth() {
-    const containerWidth = containerImg.offsetWidth;
+    containerWidth = containerImg.offsetWidth;
+    containerLeft = containerImg.offsetLeft;
 
     img1.style.width = containerWidth + 'px';
     img2.style.width = containerWidth + 'px';
@@ -17,4 +68,6 @@ document.addEventListener('DOMContentLoaded', () => {
   window.addEventListener('resize', adjustWidth)
 
   adjustWidth();
+
+  initEvents();
 })
